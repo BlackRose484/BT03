558f53b9e25e13486e09ae351677eccc41cd5dc1
hung <hungnbc2@gmai.com>
2023-03-16 08:13:19 +0700

    b2

---
+++ b2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, int> seen;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        seen[a[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (seen[a[i]] > 1)
+        {
+            cout << "Yes";
+            seen[a[i]] = 1;
+            exit(0);
+        }
+    }
+    cout << "No";
+    system("pause");
+}
